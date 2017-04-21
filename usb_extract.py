981ad710185de35f2a9e0eb8a0490ec343651c2f
pranav <pranavsanwal8@gmail.com>
2017-04-21 17:43:12 +0530

    slight improvements .

--- usb_extract.py
+++ usb_extract.py
@@ -1,6 +1,8 @@
 import sys
 import os
+from os.path import isfile,join,isdir
 import subprocess as sp
+import shutil
 import webbrowser
 import Tkinter as tk
 import tkMessageBox
@@ -9,29 +11,36 @@ import tkMessageBox
 
 def pull_operation():
 	file_name='log.txt'
-	src='//sdcard//'+file_name
+	src='//sdcard//'+ file_name
 	dest=os.path.join(os.getcwd(),'extracted')
-	if not os.path.exists(dest):
-		os.makedirs(dest)
+	if (isdir(dest)):
+		shutil.rmtree(dest)
+	os.makedirs(dest)
 	os.chdir(dest)
-	cmd = 'adb pull ' + src 
-	a = sp.Popen(cmd,shell=True)
-	webbrowser.open('log.txt')
+	cmd = 'adb pull ' + src
+	try:
+		adb = sp.Popen(cmd,shell=True)
+		adb.wait()
+		webbrowser.open(file_name)
+		list_devices()
+	except:
+		tkMessageBox.showinfo("ERROR!", "unexpected error, try again")			
+		return
 
-def detect_callback():
+def list_devices():
 	for ele in root.winfo_children():
 		ele.destroy()
-	w = tk.Button(root,text ="refresh device list",command = detect_callback)
-	w.pack()
+	refreshbutton = tk.Button(root,text ="refresh device list",command = list_devices)
+	refreshbutton.pack()
+
 	devices = check_devices()
-	if devices == 1:
+	if devices == -1:
 		return #error!!
 
 	for i in range(len(devices)):
-			w = tk.Button(root,text =devices[i],command = pull_operation)
-			w.pack()
-	#populate buttons with device name		
-	#pull_operation()	
+			displaytext = 'fetch '+ devices[i]
+			devices = tk.Button(root,text =displaytext,command = pull_operation)
+			devices.pack()
 
 def check_devices():
 	device_list = sp.check_output("adb devices",shell=True)
@@ -39,24 +48,19 @@ def check_devices():
 	if len(device_list)>4: #List of devices attached is 4
 		device_list=device_list[4:] #removes above elements
 		c = 'device'
-		if c in device_list:
-			device_list.remove(c)
-		return device_list	
-		'''count = 1
-		for i in range(count,len(a)):
-			print len(a)
-			if count%2==0:
-				del a[count-1]
-			count+=1
-		print a'''
+		c = c[::2]
+		print c
+		return device_list
+
 	else:
 		tkMessageBox.showinfo("ERROR!", "no device connected")
-		return 1
+		return -1
 
 root = tk.Tk()
 root.title("root file copy")
 root.geometry('500x400')
-w = tk.Button(root,text ="list devices",command = detect_callback)
-w.pack()
+
+listdevices = tk.Button(root,text ="list devices",command = list_devices)
+listdevices.pack()
 
 tk.mainloop()
